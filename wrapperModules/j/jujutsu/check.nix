63e31bbfe6931e1c9ef72affc7bc84b024e06c12
Birdee <85372418+BirdeeHub@users.noreply.github.com>
2026-01-11 16:46:50 -0800

    tests(wrapperModules.mpv): make tests more reliable
    These were faulty. I believe this to be because in mpv's case, grepping
    a binary was not reliable without flags
    
    Also make jujutsu's test requirements very slightly less stringent, as
    the output may differ slightly

--- wrapperModules/j/jujutsu/check.nix
+++ wrapperModules/j/jujutsu/check.nix
@@ -16,7 +16,17 @@ let
 
 in
 pkgs.runCommand "jujutsu-test" { } ''
-  "${jujutsuWrapped}/bin/jj" config list --user | grep -q 'user.name = "Test User"'
-  "${jujutsuWrapped}/bin/jj" config list --user | grep -q 'user.email = "test@example.com"'
+  if ! "${jujutsuWrapped}/bin/jj" config list --user | grep -q 'Test User'; then
+    echo "failed to list test user!"
+    echo "wrapper contents for ${jujutsuWrapped}/bin/jj"
+    cat "${jujutsuWrapped}/bin/jj"
+    exit 1
+  fi
+  if ! "${jujutsuWrapped}/bin/jj" config list --user | grep -q -F 'test@example.com'; then
+    echo "failed to list test email!"
+    echo "wrapper contents for ${jujutsuWrapped}/bin/jj"
+    cat "${jujutsuWrapped}/bin/jj"
+    exit 1
+  fi
   touch $out
 ''
--- wrapperModules/m/mpv/check.nix
+++ wrapperModules/m/mpv/check.nix
@@ -18,7 +18,17 @@ let
 
 in
 pkgs.runCommand "mpv-test" { } ''
-  "${mpvWrapped}/bin/mpv" --version | grep -q "mpv"
-  cat "${mpvWrapped.configuration.package}/bin/mpv" | grep -q "share/mpv/scripts/visualizer.lua"
+  if ! "${mpvWrapped}/bin/mpv" --version | grep -q "mpv"; then
+    echo "failed to run wrapped package!"
+    echo "wrapper content for ${mpvWrapped}/bin/mpv"
+    cat "${mpvWrapped}/bin/mpv"
+    exit 1
+  fi
+  if ! cat "${mpvWrapped.configuration.package}/bin/mpv" | LC_ALL=C grep -a -F -q "share/mpv/scripts/visualizer.lua"; then
+    echo "failed to find added script when inspecting overriden package value"
+    echo "overriden package value ${mpvWrapped.configuration.package}/bin/mpv"
+    cat "${mpvWrapped.configuration.package}/bin/mpv"
+    exit 1
+  fi
   touch $out
 ''
